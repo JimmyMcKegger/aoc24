e8847101c72eb57bc79604232c872a93f6a79f4c
James Groves <james.groves@shopify.com>
2024-12-10 23:38:12 +0000

    day 2 refactor

--- lib/day02/day02.ex
+++ lib/day02/day02.ex
@@ -2,38 +2,26 @@ defmodule Aoc.Day02 do
   @moduledoc """
   Day 02 Solutions.
   """
-  alias Aoc.Helpers
+  import Aoc.Helpers
 
-  def p1 do
-    "../inputs/d2.txt"
-    |> Helpers.read_input()
-    |> Stream.map(&to_levels/1)
-    |> Stream.filter(&safety_check/1)
-    |> Enum.count()
-  end
+  def p1,
+    do:
+      Stream.filter(input_levels(), &safety_check/1)
+      |> Enum.count()
 
-  def p2 do
-    "../inputs/d2.txt"
-    |> Helpers.read_input()
-    |> Enum.map(&to_levels/1)
-    |> Enum.filter(&dampened_safety_check/1)
-    |> Enum.count()
-  end
+  def p2,
+    do:
+      Enum.filter(input_levels(), &dampened_safety_check/1)
+      |> Enum.count()
 
   defp safety_check(level) do
     increasing = Enum.uniq(level) |> Enum.sort()
     decreasing = Enum.reverse(increasing)
 
-    cond do
-      level == increasing ->
-        gradual_check(level, :asc)
-
-      level == decreasing ->
-        gradual_check(level, :desc)
-
-      true ->
-        IO.puts("invalid")
-        false
+    if level in [increasing, decreasing] do
+      gradual_check(level)
+    else
+      false
     end
   end
 
@@ -45,23 +33,20 @@ defmodule Aoc.Day02 do
         for i <- 0..(length(level) - 1),
             do: List.delete_at(level, i)
 
-      options
-      |> Enum.any?(fn level -> safety_check(level) end)
+      Enum.any?(options, fn level -> safety_check(level) end)
     end
   end
 
-  defp gradual_check(level, :asc) do
-    level
-    |> Enum.chunk_every(2, 1, :discard)
-    |> Enum.all?(fn [x, y] -> abs(x - y) <= 3 end)
-  end
-
-  defp gradual_check(level, :desc) do
-    level
-    |> Enum.chunk_every(2, 1, :discard)
-    |> Enum.all?(fn [x, y] -> abs(x - y) <= 3 end)
-  end
+  defp gradual_check(level),
+    do:
+      Enum.chunk_every(level, 2, 1, :discard)
+      |> Enum.all?(fn [x, y] -> abs(x - y) <= 3 end)
 
   defp to_levels(line),
     do: String.split(line, " ") |> Enum.map(&String.to_integer/1)
+
+  defp input_levels(),
+    do:
+      read_input("../inputs/d2.txt")
+      |> Stream.map(&to_levels/1)
 end
