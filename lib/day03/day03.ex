273a7bea24fd6019f0fc8fe955b688af79a6ed8a
James Groves <james.groves@shopify.com>
2024-12-11 00:34:52 +0000

    day 3 part 2

--- lib/day03/day03.ex
+++ lib/day03/day03.ex
@@ -3,13 +3,37 @@ defmodule Aoc.Day03 do
   Day 03 Solutions.
   """
 
-  alias Aoc.Helpers
+  import Aoc.Helpers
 
   @pattern ~r/mul\((?<foo>\d{1,3})\,(?<bar>\d{1,3})\)/
+  @wordpattern ~r/(?<mul>mul\(\d{1,3},\d{1,3}\))|(?<do>do\(\))|(?<dont>don't\(\))/
+
+  def p2 do
+    {_state, result} =
+      read_input("../inputs/d3.txt")
+      |> Enum.flat_map(&Regex.scan(@wordpattern, &1, capture: :all_names))
+      |> List.flatten()
+      |> just_valid()
+
+    Regex.scan(@pattern, result, capture: :all_names)
+    |> Enum.map(&mult/1)
+    |> Enum.sum()
+  end
+
+  defp just_valid(list) do
+    Enum.reduce(list, {true, ""}, fn word, {state, result} ->
+      case {state, word} do
+        {true, "don't()"} -> {false, result}
+        {true, mul} -> {true, result <> mul}
+        {false, "do()"} -> {true, result}
+        {state, _} -> {state, result}
+      end
+    end)
+  end
 
   def p1,
     do:
-      Helpers.read_input("../inputs/d3.txt")
+      read_input("../inputs/d3.txt")
       |> Enum.flat_map(&Regex.scan(@pattern, &1, capture: :all_names))
       |> Enum.map(&mult/1)
       |> Enum.sum()
