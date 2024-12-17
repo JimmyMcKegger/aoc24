8220b8db72ac68d95b4df1e5aeca36c0a8566e55
James <jamesrobertsongroves@gmail.com>
2024-12-17 01:03:29 +0000

    d11-refactor

--- lib/day11/d11.ex
+++ lib/day11/d11.ex
@@ -2,69 +2,79 @@ defmodule Aoc.D11 do
   import Aoc.Helpers, only: [read_input: 1, parse_integers: 1]
   require Integer
 
-  @stone_rules %{
-    0 => 1,
-    1 => 2024,
-    10 => [1, 0],
-    99 => [9, 9],
-    999 => 2021976
-  }
+  @input "../inputs/d11.txt"
 
-  def p1 do
-    blinks = 25
+  def start_cache() do
+    :ets.new(:stone_cache, [:set, :public, :named_table])
 
-    read_input("../inputs/d11.txt")
+    :ets.insert(:stone_cache, [
+      {0, 1},
+      {1, 2024},
+      {10, [1, 0]},
+      {99, [9, 9]},
+      {999, 2_021_976}
+    ])
+  end
+
+  def p1(blinks \\ 25, input \\ @input) do
+
+    case :ets.whereis(:stone_cache) do
+      :undefined -> start_cache()
+      _ -> true
+    end
+
+    read_input(input)
     |> hd()
     |> String.split(" ")
     |> parse_integers()
     |> IO.inspect()
-    |> blink(blinks, @stone_rules)
+    |> blink(blinks)
     |> Enum.count()
-
   end
 
-  def blink(stones, 0, _cache), do: stones
+  def blink(stones, 0), do: List.flatten(stones)
 
-  def blink(stones, count, cache) do
+  def blink(stones, count) do
+    IO.inspect(count, label: "COUNT")
 
-    Enum.map(stones, fn num ->
-      new_stone = cache[num]
+    new_stones =
+      Enum.map(stones, &process_stone/1)
+      |> List.flatten()
 
-      cond do
-        new_stone ->
-          new_stone
-        true ->
-          # calculate and update cache
-          new_stones = handle_new_number(num)
-          cache = Map.put(cache, num, new_stones)
-          new_stones
-      end
+    blink(new_stones, count - 1)
+  end
 
-    end)
-    |> List.flatten()
-    |> blink(count - 1, cache)
+  def process_stone(num) do
+    case :ets.lookup(:stone_cache, num) do
+      [{^num, existing_result}] ->
+        existing_result
+
+      [] ->
+        new_result = handle_new_number(num)
+        :ets.insert(:stone_cache, {num, new_result})
+        new_result
+    end
   end
 
   def handle_new_number(num) do
-    digit_count = num |> Integer.digits() |> length
+    digit_count = :math.log10(num) |> trunc()
 
-    calculate_new_stones(num, digit_count)
+    calculate_new_stones(num, digit_count + 1)
   end
 
   def calculate_new_stones(num, digit_count) when Integer.is_even(digit_count) do
-
-      num
-      |> Integer.to_charlist()
-      |> Enum.chunk_every(div(digit_count, 2))
-      |> Enum.map(fn chars ->
-        to_string(chars)
-        |> String.to_integer()
-      end)
+    num
+    |> Integer.to_charlist()
+    |> Enum.chunk_every(div(digit_count, 2))
+    |> Enum.map(fn chars ->
+      to_string(chars)
+      |> String.to_integer()
+    end)
   end
 
   def calculate_new_stones(num, _digit_count) do
-      num * 2024
+    num * 2024
   end
 
-  def p2, do: 0
+  def p2, do: p1(75, @input)
 end
--- test/aoc_test.exs
+++ test/aoc_test.exs
@@ -1,8 +1,18 @@
 defmodule AocTest do
   use ExUnit.Case
-  doctest Aoc
 
-  test "greets the world" do
-    assert Aoc.hello() == :world
+  test "Day 1 part 1" do
+    assert Aoc.Day01.solve_part1("../inputs/d1sample.txt") == 11
+  end
+
+  test "Day 1 part 2" do
+    assert Aoc.Day01.solve_part2("../inputs/d1sample.txt") == 31
+  end
+
+  test "Day 11 part 1" do
+    assert Aoc.D11.p1(
+             6,
+             "../inputs/d11sample.txt"
+           ) == 22
   end
 end
