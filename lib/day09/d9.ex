38a46a5f6749feaf1021dbb59b9a5e5de8cc23d0
James <jamesrobertsongroves@gmail.com>
2024-12-26 11:11:19 +0000

    d9 p1

--- lib/day09/d9.ex
+++ lib/day09/d9.ex
@@ -1,24 +1,114 @@
 defmodule Aoc.D9 do
   import Aoc.Helpers, only: [read_input: 1, parse_integers: 1]
 
+  def start_cache() do
+    :ets.new(:disk_drive, [:set, :public, :named_table])
+  end
+
+  def cache_size(),
+    do: :ets.info(:disk_drive) |> Keyword.get(:size)
+
+    @doc ~S"""
+    Finds the filesystem's checksum
+
+    ## Examples
+
+      iex> Aoc.D9.p1()
+      1928
+    """
   def p1(input \\ "../inputs/d9sample.txt") do
+    case :ets.whereis(:disk_drive) do
+      :undefined -> start_cache()
+      _ -> true
+    end
+
     read_input(input)
     |> hd()
-    |> String.split("", trim: true)
+    |> String.graphemes()
     |> parse_integers()
-    |> to_disk()
+    |> IO.inspect()
+    |> handle_list()
+
+    fill_gaps()
+
   end
 
-  defp to_disk(list) do
-    list
-    |> Enum.chunk_every(2)
-    |> Enum.with_index(fn chunk, i -> to_filespace({Integer.to_string(i), chunk}) end)
-    |> Enum.join("")
+  def fill_gaps() do
+    last_position = cache_size() - 1
+
+    Enum.reduce_while(0..last_position, 0, fn position, acc ->
+      handle_gaps(position, acc, last_position)
+    end)
   end
 
-  defp to_filespace({i, [file]}), do: to_filespace({i, [file, 0]})
 
-  defp to_filespace({i, [file_size, empty]}) do
-    "#{String.duplicate(i, file_size)}#{String.duplicate(".", empty)}"
+  def handle_gaps(position, acc, last_position) do
+    case :ets.lookup(:disk_drive, position) do
+      [{^position, nil}] ->
+        {index_to_move, swap_value} = get_last_data(last_position)
+
+        if index_to_move <= position do
+          # no more data to move
+          {:halt, acc}
+        else
+          :ets.delete(:disk_drive, index_to_move)
+          :ets.insert(:disk_drive, {position, swap_value})
+
+          IO.inspect("MOVING #{swap_value} from index #{index_to_move} to #{position}")
+          {:cont, acc + (position * swap_value)}
+        end
+
+      [{^position, value}] ->
+        # Already compacted
+        {:cont, acc + (position * value)}
+
+      _ ->
+        {:cont, acc}
+    end
+  end
+
+  def get_last_data(last_position) do
+    find_last_valid(last_position)
+  end
+
+  defp find_last_valid(pos) when pos < 0 do
+    # no more data
+    {:halt, nil}
+  end
+
+  defp find_last_valid(pos) do
+    case :ets.lookup(:disk_drive, pos) do
+      [{^pos, nil}] ->
+        find_last_valid(pos - 1)
+
+      [{^pos, value}] ->
+        {pos, value}
+
+      _ ->
+        find_last_valid(pos - 1)
+    end
+  end
+
+
+  def handle_list(list) do
+    Enum.reduce(list, %{data: true, file_id: 0}, &disk_map(&1, &2))
+  end
+
+  def disk_map(files, %{data: true, file_id: file_id}) do
+    for new_space <- cache_size()..((cache_size() + files) - 1),
+        do: :ets.insert(:disk_drive, {new_space, file_id})
+
+    %{data: false, file_id: file_id + 1}
+  end
+
+  def disk_map(0, %{data: false, file_id: file_id}) do
+    %{data: true, file_id: file_id}
+  end
+
+  def disk_map(files, %{data: false, file_id: file_id}) do
+    for new_space <- cache_size()..((cache_size() + files) - 1),
+        do: :ets.insert(:disk_drive, {new_space, nil})
+
+    %{data: true, file_id: file_id}
   end
 end
--- test/aoc_test.exs
+++ test/aoc_test.exs
@@ -3,6 +3,7 @@ defmodule AocTest do
 
   doctest Aoc.D4
   doctest Aoc.D7
+  doctest Aoc.D9
 
   test "Day 1 part 1" do
     assert Aoc.Day01.solve_part1("../inputs/d1sample.txt") == 11
@@ -12,11 +13,11 @@ defmodule AocTest do
     assert Aoc.Day01.solve_part2("../inputs/d1sample.txt") == 31
   end
 
-  test "Day 11 part 1" do
-    assert Aoc.D11.p1(
-             6,
-             "../inputs/d11sample.txt"
-           ) == 22
-  end
+  # test "Day 11 part 1" do
+  #   assert Aoc.D11.p1(
+  #            6,
+  #            "../inputs/d11sample.txt"
+  #          ) == 22
+  # end
 
 end
