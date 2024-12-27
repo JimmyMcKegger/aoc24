1c3c1752157877c13b6edba4d92e0cc8f0d2c9c2
James <jamesrobertsongroves@gmail.com>
2024-12-27 17:29:05 +0000

    day 25 p1

--- lib/day25/d25.ex
+++ lib/day25/d25.ex
@@ -7,10 +7,18 @@ defmodule Aoc.D25 do
       |> Enum.split_with(fn {l, _n} -> l == :key end)
 
     every_combo =
-      for k <- keys, l <- locks, do: [k, l]
+      for k <- keys, l <- locks, do: {no_overlaps?(k, l), k, l}
 
-
+		Enum.count(every_combo, fn {answer, _key_tup, _lock_tup} -> answer end)
   end
+
+	defp no_overlaps?({:key, key_values}, {:lock, lock_values}) do
+		[key_values, lock_values]
+			|> Enum.map(&Tuple.to_list/1)
+			|> Enum.zip()
+			|> Enum.map(fn {a, b} -> a + b end)
+			|> Enum.all?(&(&1 <= 5))
+	end
 
   defp read_inputs(file_path) do
     file_path
@@ -22,13 +30,14 @@ defmodule Aoc.D25 do
 
   defp to_lock_or_key([head | tail]) do
     if head == "#####" do
-      make_key(tail)
+      tally_up(tail, :lock)
     else
-      make_lock(tail)
+			key_data = Enum.take([ head | tail], 6)
+      tally_up(key_data, :key)
     end
   end
 
-  defp make_key(list) do
+  defp tally_up(list, label) do
     Enum.reduce(list, {0, 0, 0, 0, 0}, fn line, acc ->
       String.graphemes(line)
       |> Enum.map(
@@ -41,23 +50,7 @@ defmodule Aoc.D25 do
       |> List.to_tuple()
       |> positional_adder(acc)
     end)
-    |> labeler(:key)
-  end
-
-  defp make_lock(list) do
-    Enum.reduce(list, {0, 0, 0, 0, 0}, fn line, acc ->
-      String.graphemes(line)
-      |> Enum.map(
-        &if &1 == "#" do
-          1
-        else
-          0
-        end
-      )
-      |> List.to_tuple()
-      |> positional_adder(acc)
-    end)
-    |> labeler(:lock)
+    |> labeler(label)
   end
 
   defp positional_adder({a1, b1, c1, d1, e1}, {a2, b2, c2, d2, e2}) do
